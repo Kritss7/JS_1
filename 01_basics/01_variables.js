3a966715f50302d09ee45391db64da2a1f17b0b1
Kritss7 <165828248+Kritss7@users.noreply.github.com>
2025-07-12 19:01:06 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID = 12233
+let accountEmail= "abcd@gmail.com"
+var accountPassword = "12344"
+accountCity = "Jaipur"
+
+accountEmail= "assd@gmail.com"
+
+console.log(accountEmail);
+console.table([accountID, accountEmail, accountPassword, accountCity])
