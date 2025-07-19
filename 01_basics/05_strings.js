820ec082a541d1b61c65628e967b0fa46d7ae6aa
Kritss7 <165828248+Kritss7@users.noreply.github.com>
2025-07-19 21:01:57 +0000

    container config done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,26 @@
+// stack (primitive), Heap(non primitive)
+
+const fname= "kritika"
+const sname= "srivastava"
+// console.log(fname + sname + "dear");
+
+// better way to write above things
+
+// console.log(`hello my name is ${fname} and my surname is ${sname}`);
+
+const gameName= "anneWithanE"
+console.log(gameName[0]);
+
+console.log(gameName.charAt(2));
+
+const newstring= gameName.substring(0,4)
+console.log(newstring);
+
+// const anotherstring = gameName.slice(-8, 4)
+// console.log(anotherstring);
+
+const url= "https://free-url-shortener.rb.gy/"
+console.log(url.replace('free', '-'));
+console.log(url.includes('free'));
+console.log(gameName.split('-'));
+
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,9 @@
+const score= 400
+
+console.log(score);
+const balance = new Number(200)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+
