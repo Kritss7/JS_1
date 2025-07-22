375e63a057e8535d6f95ddafab19d47a6741637a
Kritss7 <165828248+Kritss7@users.noreply.github.com>
2025-07-22 22:31:18 +0000

    container config done

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -5,5 +5,11 @@ const balance = new Number(200)
 console.log(balance);
 
 console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(balance.toFixed(1));
 
+const n= 23.899898
+const hundred= 100000
+console.log(n.toPrecision(3));
+console.log(hundred.toLocaleString());
 
